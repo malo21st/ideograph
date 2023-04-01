2d07cc324eaa0a99de9b0442f320a8c591817daf
malo21st <44739330+malo21st@users.noreply.github.com>
2023-04-01 15:35:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,12 @@ nodes ,edges = [], []
 config = Config(width=750, height=950, directed=False, physics=True, hierarchical=False)
 nodes.append(Node(id=0, label="0", size=25))
 
-if i: = (st.session_state['count'] < 5):
+if st.session_state['count'] < 5:
+    st.session_state['count'] += 1
+    i = st.session_state['count']
     st.write(i)
     nodes.append(Node(id=i, label=f"{i}", size=25))
     edges.append(Edge(source=0, target=i))
     agraph(nodes=nodes, edges=edges, config=config)
-    st.session_state['count'] += 1
     time.sleep(3)
 # return_value = agraph(nodes=nodes, edges=edges, config=config)
