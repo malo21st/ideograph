5e8962d2c6ad57d9d61a9cb48a36afb343f2c147
malo21st <44739330+malo21st@users.noreply.github.com>
2023-04-01 22:35:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from streamlit_agraph import agraph, Node, Edge, Config
 import numpy as np
 import random
 
+st.session_state['first_time'] = True
+
 def generate_edge_lst(size = 100):
     generator = np.random.default_rng()
     rnd = generator.normal(size=size)
@@ -17,20 +19,23 @@ def generate_edge_lst(size = 100):
             edge_lst.append(((node, random.choice(node_dic[node])), (node+1, node_dic[node+1][-1])))
     return edge_lst
 
+if st.session_state['edge_lst'] is None:
+    st.session_state['edge_lst'] = generate_edge_lst()
+    st.session_state['edges'] = list()
+
 # layout
 st.sidebar.header("AI Mind Map")
 theme = st.sidebar.text_input("**お題を入力してください :**")
 
-if theme:
-    st.session_state['edge_lst'] = generate_edge_lst()
+if theme and st.session_state['first_time']:
     st.session_state['nodes'] = [Node(id=str((0, 1)), label=theme, size=10)]
-    st.session_state['edges'] = list()
     config = Config(width=750, height=750, directed=False, physics=True, hierarchical=False)
-    return_value = agraph(nodes=st.session_state['nodes'], edges=st.session_state['edges'], config=config)
+    agraph(nodes=st.session_state['nodes'], edges=st.session_state['edges'], config=config)
+    st.session_state['first_time'] = False
 
 if st.sidebar.button("PUSH"):
     src, tgt = st.session_state['edge_lst'].pop(0)
     st.session_state['nodes'].append(Node(id=f"{tgt}", label=f"{tgt}", size=5))
     st.session_state['edges'].append(Edge(source=f"{src}", target=f"{tgt}"))
     config = Config(width=750, height=750, directed=False, physics=True, hierarchical=False)
-    return_value = agraph(nodes=st.session_state['nodes'], edges=st.session_state['edges'], config=config)
+    agraph(nodes=st.session_state['nodes'], edges=st.session_state['edges'], config=config)
