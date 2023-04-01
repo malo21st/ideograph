bbf951a95ef2a6cfa848fdd2bfe076f30ccbfae8
malo21st <44739330+malo21st@users.noreply.github.com>
2023-04-01 16:02:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 
 if 'count' not in st.session_state:
     st.session_state['count'] = 0
-    st.session_state['nodes'] = [Node(id=0, label="0", size=25)]
+    st.session_state['nodes'] = [Node(id=0, label="0", size=10)]
     
 # if 'nodes' not in st.session_state:
 #     st.session_state['nodes'] = list()
@@ -12,12 +12,13 @@ if 'count' not in st.session_state:
 if 'edges' not in st.session_state:
     st.session_state['edges'] = list()
     
-if st.button("PUSH"):
+# if st.button("PUSH"):
+for st.session_state['count'] < 10:
     st.session_state['count'] += 1
     i = st.session_state['count']
-    st.session_state['nodes'].append(Node(id=i, label=f"{i}", size=25))
+    st.session_state['nodes'].append(Node(id=i, label=f"{i}", size=5))
     st.session_state['edges'].append(Edge(source=0, target=i))
     config = Config(width=500, height=500, directed=False, physics=True, hierarchical=False)
     agraph(nodes=st.session_state['nodes'], edges=st.session_state['edges'], config=config)
-#     time.sleep(3)
+    time.sleep(2)
 # return_value = agraph(nodes=nodes, edges=edges, config=config)
