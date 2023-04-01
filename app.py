2e59453e1b4bcea862c3577a1b73f1998115f68a
malo21st <44739330+malo21st@users.noreply.github.com>
2023-04-01 18:51:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ if 'count' not in st.session_state:
     st.session_state['count'] = 0
     st.session_state['nodes'] = [Node(id=0, label="0", size=10)]
     st.session_state['edges'] = list()
-    config = Config(width=500, height=500, directed=False, physics=True, hierarchical=False)
+    config = Config(width=750, height=750, directed=False, physics=True, hierarchical=False)
     return_value = agraph(nodes=st.session_state['nodes'], edges=st.session_state['edges'], config=config)
 
 if st.sidebar.button("PUSH"):
@@ -15,5 +15,5 @@ if st.sidebar.button("PUSH"):
     src_node = random.choice(st.session_state['nodes'])
     st.session_state['nodes'].append(Node(id=tgt, label=f"{tgt}", size=5))
     st.session_state['edges'].append(Edge(source=src_node.id, target=tgt))
-    config = Config(width=500, height=500, directed=False, physics=True, hierarchical=False)
+    config = Config(width=750, height=750, directed=False, physics=True, hierarchical=False)
     return_value = agraph(nodes=st.session_state['nodes'], edges=st.session_state['edges'], config=config)
