18f364539e7bb1701ae6a2e923d004c71b80dd37
malo21st <44739330+malo21st@users.noreply.github.com>
2023-04-01 16:05:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ if 'edges' not in st.session_state:
     st.session_state['edges'] = list()
     
 # if st.button("PUSH"):
-for i in range(1, 10):
-    st.session_state['count'] = i
-#     i = st.session_state['count']
-    st.session_state['nodes'].append(Node(id=i, label=f"{i}", size=5))
-    st.session_state['edges'].append(Edge(source=0, target=i))
-    config = Config(width=500, height=500, directed=False, physics=True, hierarchical=False)
-    agraph(nodes=st.session_state['nodes'], edges=st.session_state['edges'], config=config)
-    time.sleep(2)
+# for i in range(1, 10):
+st.session_state['count'] += 1
+i = st.session_state['count']
+st.session_state['nodes'].append(Node(id=i, label=f"{i}", size=5))
+st.session_state['edges'].append(Edge(source=0, target=i))
+config = Config(width=500, height=500, directed=False, physics=True, hierarchical=False)
+agraph(nodes=st.session_state['nodes'], edges=st.session_state['edges'], config=config)
+time.sleep(2)
 # return_value = agraph(nodes=nodes, edges=edges, config=config)
