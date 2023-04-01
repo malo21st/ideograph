7c3073082dd76b51486ebf194ed284b7d10aa11f
malo21st <44739330+malo21st@users.noreply.github.com>
2023-04-01 15:33:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ import streamlit as st
 from streamlit_agraph import agraph, Node, Edge, Config
 import time
 
+if 'count' not in st.session_state:
+    st.session_state['count'] = 0
+    
 nodes ,edges = [], []
 config = Config(width=750, height=950, directed=False, physics=True, hierarchical=False)
 nodes.append(Node(id=0, label="0", size=25))
-for i in range(1, 5):
+
+if i: = (st.session_state['count'] < 5):
+    st.write(i)
     nodes.append(Node(id=i, label=f"{i}", size=25))
     edges.append(Edge(source=0, target=i))
     agraph(nodes=nodes, edges=edges, config=config)
-    st.write(i)
+    st.session_state['count'] += 1
     time.sleep(3)
 # return_value = agraph(nodes=nodes, edges=edges, config=config)
