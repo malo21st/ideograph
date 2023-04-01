1e7152e159edcd37ce452d31b3ffc0ea3eb187e2
malo21st <44739330+malo21st@users.noreply.github.com>
2023-04-01 15:11:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,33 @@
 import streamlit
 from streamlit_agraph import agraph, Node, Edge, Config
+import time
 
 nodes = []
 edges = []
-nodes.append( Node(id="pref01", 
-                   label="東京", 
+
+nodes.append( Node(id=0, 
+                   label="0", 
                    size=25, 
-#                    shape="circularImage",
-#                    image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png"
                   )
             ) # includes **kwargs
-nodes.append( Node(id="pref02", 
-                   label="福岡", 
-                   size=25,
-#                    shape="circularImage",
-#                    image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png"
-                  )
-            )
-edges.append( Edge(source="pref01", 
-#                    label="friend_of", 
-                   target="pref02", 
-                   # **kwargs
-                   ) 
-            ) 
-edges.append( Edge(source="pref01", 
-#                    label="friend_of", 
-                   target="pref01", 
-                   # **kwargs
-                   ) 
-            ) 
+
+for i in range(1, 5):
+    nodes.append( Node(id=i, 
+                       label=f"{i}", 
+                       size=25,
+                      )
+                )
+    edges.append( Edge(source=0, 
+                       target=i, 
+                       ) 
+                )
+    time.sleep(3)
 
 config = Config(width=750,
                 height=950,
                 directed=False, 
                 physics=True, 
                 hierarchical=False,
-                # **kwargs
                 )
 
 return_value = agraph(nodes=nodes, 
