31f314912c9b35ccd56a37d9cf6133df307aa967
malo21st <44739330+malo21st@users.noreply.github.com>
2023-04-01 14:30:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ edges = []
 nodes.append( Node(id="Spiderman", 
                    label="Peter Parker", 
                    size=25, 
-                   shape="circularImage",
-                   image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png") 
+#                    shape="circularImage",
+#                    image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png") 
             ) # includes **kwargs
 nodes.append( Node(id="Captain_Marvel", 
                    size=25,
-                   shape="circularImage",
-                   image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png") 
+#                    shape="circularImage",
+#                    image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png") 
             )
 edges.append( Edge(source="Captain_Marvel", 
                    label="friend_of", 
