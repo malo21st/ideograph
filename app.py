a05a398a1d5023c6c85b50323394eb52cbd7f817
malo21st <44739330+malo21st@users.noreply.github.com>
2023-04-01 15:22:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,13 @@ import streamlit
 from streamlit_agraph import agraph, Node, Edge, Config
 import time
 
-nodes = []
-edges = []
-
-config = Config(width=750, height=950,
-                directed=False, physics=True, hierarchical=False,
-                )
-
+nodes ,edges = [], []
+config = Config(width=750, height=950, directed=False, physics=True, hierarchical=False)
 nodes.append(Node(id=0, label="0", size=25))
-
 for i in range(1, 5):
     nodes.append(Node(id=i, label=f"{i}", size=25))
-    edges.append( Edge(source=0, target=i))
-    time.sleep(3)
+    edges.append(Edge(source=0, target=i))
     agraph(nodes=nodes, edges=edges, config=config)
-
+    st.write(i)
+    time.sleep(3)
 # return_value = agraph(nodes=nodes, edges=edges, config=config)
